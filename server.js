d96da073315bb9b57d6313262e8ac9046a516a17
kerlenmelo <kerlen_1@hotmail.com>
2025-01-31 23:27:18 -0300

    chore: configurar integração das rotas no server.js

--- server.js
+++ server.js
@@ -5,10 +5,39 @@ const port = 3000;
 // Middleware para interpretar JSON no corpo das requisições
 app.use(express.json())
 
-// Rota inicial
+// Importa as rotas que serão utilizadas
+const todoRoutes = require('./routes/todoRoutes.js')
+app.use(todoRoutes)
+
+// Rota inicial (teste)
 app.get('/', (req, res) => {
     res.send('API To-Do List está rodando!')
 })
+
+// Conexão com o banco de dados
+const sequelize = require('./database/database'); // Importa a conexão do banco
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados estabelecida com sucesso!');
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    });
+;
+
+// Modelo do Todo
+const Todo = require('./models/Todo'); // Importa o modelo Todo
+
+// Sincroniza o banco de dados e cria a tabela se ela não existir
+sequelize.sync({ alter: true })
+    .then(() => {
+        console.log('Banco de dados sincronizado com sucesso!');
+    })
+    .catch(err => {
+        console.error('Erro ao sincronizar banco de dados:', err);
+    });
+;
 
 // Iniciar o servidor
 app.listen(port, () => {
