bd478d2843e648c5f7c05199785555c4f8be5cd7
kerlenmelo <kerlen_1@hotmail.com>
2025-01-31 23:25:38 -0300

    feat: implementar lógica de CRUD no Controller

---
+++ controllers/todoController.js
@@ -0,0 +1,94 @@
+const Todo = require('../models/Todo'); // importa o modelo
+
+// Buscar todas as tarefas
+exports.getAllTodos = async (req, res) => {
+    try {
+        const todos = await Todo.findAll();
+        
+        if (todos.length === 0) {
+            return res.status(200).json([])
+        } 
+        res.json(todos)
+        
+    } catch (error) {
+        console.error('Erro ao buscar todas as tarefas:', error)
+        res.status(500).json({ error: "Erro interno no servidor" })
+    }
+    
+}
+
+// Buscar uma tarefa específica
+exports.getTodoByID = async (req, res) => {
+    try {
+        const { id } = req.params
+        const todo = await Todo.findByPk(id);
+
+        if (!todo) {
+            return res.status(404).json({ error: "Tarefa não encontrada!"})
+        }
+        res.status(200).json(todo)
+
+    } catch (error) {
+        console.error('Erro ao buscar tarefa:', error)
+        res.status(500).json({ error: "Erro interno no servidor" })
+    }
+}
+
+// Criar uma tarefa
+exports.createTodo = async (req, res) => {
+    try {
+        const { title } = req.body;
+        
+        // Validação: título é obrigatório
+        if (!title) {
+            return res.status(400).json({ error: "Erro ao criar tarefa. O título é obrigatório!"})
+        }
+        
+        // Criar tarefa no banco
+        const novoTodo = await Todo.create({ title })
+        res.status(201).json(novoTodo)
+        
+    } catch (error) {
+        console.error('Erro ao criar tarefa:', error)
+        res.status(500).json({ error: "Erro interno no servidor" })
+    }
+}
+
+// Atualiza uma tarefa
+exports.updateTodo = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { title, completed } = req.body
+
+        const todo = await Todo.findByPk(id)
+    
+        if (!todo) {
+            return res.status(404).json({ error: "Erro ao atualizar tarefa. Tarefa não encontrada!"})
+        }
+
+        // Atualiza tarefa no banco
+        await todo.update({ title, completed })
+        res.status(200).json(todo)
+        
+    } catch (error) {
+        console.error('Erro ao atualizar tarefa:', error)
+        res.status(500).json({ error: "Erro interno no servidor" })
+    }
+}
+
+// Deletar uma tarefa
+exports.deleteTodo = async (req, res) => {
+    try {
+        const { id } = req.params
+        const todo = await Todo.findByPk(id)
+
+        if (!todo) {
+            return res.status(404).json({ error: "Erro ao deletar tarefa. Tarefa não encontrada!"})
+        }
+        await todo.destroy()
+        res.status(200).json({ message: "Tarefa deletada!"})
+    } catch (error) {
+        console.error('Erro ao deletar tarefa:', error)
+        res.status(500).json({ error: "Erro interno no servidor" })
+    }
+}
